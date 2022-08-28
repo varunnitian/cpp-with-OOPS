254550b76fff2f7790cb602f6995dfe25187005b
varunnitian <98322627+varunnitian@users.noreply.github.com>
2022-08-28 11:54:53 +0530

    Add files via upload

---
+++ c++part2/practice2.cpp
@@ -0,0 +1,104 @@
+// #include<iostream>
+// using namespace std;
+// int add(int x,int y){
+//     int z;
+//     z=x+y;
+//     return z;
+// }
+// int main(){
+// int x,y,z;
+// z=add(10,40);
+// cout<<z;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int add(int x,int y){
+//     return x+y;
+// }
+// int main(){
+// int x,y,z;
+// z=add(10,40);
+// cout<<z;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int swap(int a,int b){
+//     int temp;
+//     temp=a;
+//     a=b;
+//     b=temp;
+// }
+// int main(){
+// int x=10,y=20;
+// swap(x,y);
+// cout<<x<<" "<<y;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int swap(int a,int b){
+//     int temp;
+//     temp=a;
+//     a=b;
+//     b=temp;
+//     return temp;
+// }
+// int main(){
+// int x=10,y=20,temp;
+// temp=x;
+// x=y;
+// y=temp;
+// swap(x,y);
+// cout<<x<<" "<<y;
+//     return 0;
+// }
+// #include<iostream>
+// int swap(int x,int y){
+//     int temp;
+//     temp=x;
+//     x=y;
+//     y=temp;
+//     return temp;
+// }
+// using namespace std;
+// int main(){
+//     int x,y,z;
+//     z=max(299,20);
+//     cout<<"maximum is :"<<z;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int g=0;
+// void fun()
+// {
+//     int a=5;
+//     g+=a;
+//     cout<<g<<" ";
+// }
+
+
+// int main()
+// {
+//     int x=10;
+// g=15;
+// fun();
+// g++;
+// cout<<g;
+// return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int multiply(int x,int y){
+//     int z;
+//     z=x*y;
+//     return z;
+// }
+// int main(){
+// int x,y,z;
+// z=multiply(20,5);
+// cout<<"multiplication is:"<<z<<endl;
+//     return 0;
+// }
