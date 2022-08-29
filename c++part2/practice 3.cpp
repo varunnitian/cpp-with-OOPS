314ded02cbdbab80219662dd41ac6b6438c1fd8c
varunnitian <98322627+varunnitian@users.noreply.github.com>
2022-08-29 22:43:34 +0530

    Add files via upload

---
+++ c++part2/practice 3.cpp
@@ -0,0 +1,73 @@
+// multiplication of two numbers//
+
+// #include<iostream>
+// using namespace std;
+// int multiply(int x,int y){
+//     int z;
+//     z=x*y;
+//     return z;
+// }
+// int main(){
+// int x,y,z;
+// z=multiply(20,5);
+// cout<<"multiplication is:"<<z<<endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// void fun(int x){
+// x=60;
+// }
+// int main(){
+// int x=20;
+// fun(x);
+// cout<<"x="<<x<<endl;
+//     return 0;
+// }                 //function definition-pass by reference//
+// #include<iostream>
+// using namespace std;
+// void fun(int *ptr)
+// {
+//     *ptr=30;
+// }
+// int main(){
+// int x=20;
+// fun(&x);
+// cout<<"x="<<x<<endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// void function(int a)
+// {
+//     .....
+//     return ;
+// }
+// int main(){
+
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// void fun(void)
+// int main(){
+
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int swap(int a,int b)
+// {
+// int temp;
+// temp=a;
+// a=b;
+// b=temp;
+// return temp;
+// }
+// int main()
+// {
+// int a=20,b=30,z;
+// z=swap(a,b);
+// cout<<z;
+//     return 0;
+// }
