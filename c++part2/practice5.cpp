6d3ee3f333885f369309d2ee8f01fb12177b8be2
varunnitian <98322627+varunnitian@users.noreply.github.com>
2022-08-30 18:08:55 +0530

    Add files via upload

---
+++ c++part2/practice5.cpp
@@ -0,0 +1,27 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a=10;
+//     int &r=a;
+//     // r++;
+//     cout<<a<<endl;
+//     // r++;
+//     cout<<r<<endl;
+//     // r++;
+//     // cout<<&r<<endl;
+//     // cout<<&a<<endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int length=0,breadth=0;
+//     cout<<"enter length and breadth"<<endl;
+//     cin>>length>>breadth;
+//     int area=length*breadth;
+//     int peri=2*(length+breadth);
+//     cout<<" area and perimeter are:"<<area<<" "<<peri<<endl;
+//     return 0;
+// }
