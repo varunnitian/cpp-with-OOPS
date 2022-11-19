351b1e5cc15dac194cf5af3ffa209af158601a92
varun jain <98322627+varunnitian@users.noreply.github.com>
2022-11-19 17:55:11 +0530

    Added code for c++ 11

---
+++ c++part2/c++11.cpp
@@ -0,0 +1,38 @@
+//program for auto//
+
+// #include<iostream>
+// using namespace std;
+// float fun(){
+//     return 2.56f;
+// }
+// int main(){
+// double d=12.3f;
+// int i=9;
+// auto x=2*d+i;
+// cout<<x;
+//     return 0;
+// }
+//program for final keyword//
+// #include<iostream>
+// using namespace std;
+// class parent{
+//     public:
+//     virtual void show() final{}
+// };
+// class child:parent{
+//     void show() {} 
+// };
+// int main(){
+
+//     return 0;
+// }
+//program for lamda expression//
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     [](int x,int y){
+//         cout<<"sum is: "<<x+y<<endl;
+//     }
+//     (10,20);
+//     return 0;
+// }
