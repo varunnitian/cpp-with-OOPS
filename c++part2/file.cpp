6e3da3cbd96862c9fe2ead729c3de4e0ebc9a0e5
varun jain <98322627+varunnitian@users.noreply.github.com>
2022-11-19 17:56:02 +0530

    Added code for file

---
+++ c++part2/file.cpp
@@ -0,0 +1,34 @@
+// #include<iostream>
+// #include<fstream>
+// using namespace std;
+// int main(){
+// ofstream ofs("My.txt",ios::trunc);
+// ofs<<"john"<<endl;
+// ofs<<25<<endl;
+// ofs<<"cs"<<endl;
+// ofs.close();
+//     return 0;
+// }
+// #include<iostream>
+// #include<fstream>
+// using namespace std;
+// int main(){
+// ifstream infile;
+// infile.open("my.txt");
+// string str;
+// int x;
+// infile>>str;
+// infile>>x;
+// cout<<str<<" "<<x<<endl;
+// if (infile.eof())
+// {
+//     cout<<"end of the file reached"<<endl;
+// file.close();
+// }
+
+    // ifstream infile;
+    // infile.open("my.txt");
+    // if(!infile.is_open()){
+    // cout<<"file cannot be opened";
+    // }
+// }
