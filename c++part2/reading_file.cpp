1404210af341d6660d40eb0c368586c23e273be8
varun jain <98322627+varunnitian@users.noreply.github.com>
2022-11-20 17:40:36 +0530

    Added code for file i/o

---
+++ c++part2/reading_file.cpp
@@ -0,0 +1,24 @@
+
+//reading in file//
+// #include<iostream>
+// #include<fstream>
+// using namespace std;
+// int main(){
+//     ifstream ifs;
+//     ifs.open("my.txt");
+//     if (ifs)
+//     {
+//         cout<<"file is opened"<<endl;
+//     }
+    
+//     string name;
+//     int roll;
+// string branch;
+// ifs>>name>>branch>>roll;
+// // ifs.close();
+// cout<<"name: "<<name<<endl;
+// cout<<"roll_no:"<<roll<<endl;
+// cout<<"branch:"<<branch<<endl;
+// ifs.close();
+//     return 0;
+// }
