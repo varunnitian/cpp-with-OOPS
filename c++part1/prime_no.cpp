d0727752d000162722f363889b65a9657adabe14
varun jain <21bch002@nith.ac.in>
2023-01-03 16:08:44 +0530

    Update prime_no.cpp

--- c++part1/prime_no.cpp
+++ c++part1/prime_no.cpp
@@ -1,24 +1,49 @@
 #include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int count=0;
-    for (int i = 1; i <=n; i++)
-    {
-        if (n%i==0)
-        {
-            count++;
-        }
-    }
-    if (count==2)
-        {
-            cout<<"the given number is a prime number"<<endl;
-        }
-     else
-        {
-            cout<<"the given number is not a prime number"<<endl;
-        }
+// using namespace std;
+// bool isprime(int n){
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//             return false;
+//         }
+//         else{
+//             return true;
+//         }
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     if(isprime(n)){
+//         cout<<"prime number"<<endl;
+//     }
+//     else{
+//         cout<<"not a prime number"<<endl;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=0;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         if (n%i==0)
+//         {
+//             count++;
+//         }
+//     }
+//     if (count==2)
+//         {
+//             cout<<"the given number is a prime number"<<endl;
+//         }
+//      else
+//         {
+//             cout<<"the given number is not a prime number"<<endl;
+//         }
     
-    return 0;
-}
+//     return 0;
+// }
